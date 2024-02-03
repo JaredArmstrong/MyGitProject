8d11e277b240042912a3520dbfb455c1cfe11d61
JaredArmstrong <158697733+JaredArmstrong@users.noreply.github.com>
2024-02-03 19:35:17 -0400

    Create change.py
    used to break down a value taken into the lowest amount of change

---
+++ change.py
@@ -0,0 +1,20 @@
+def get_change(input);
+
+#value fo bills and coins in cents
+denominations = {
+        10000: "100 dollar bill",
+        5000: "50 dollar bill",
+        2000: "20 dollar bill",
+        1000: "10 dollar bill",
+        500: "5 dollar bill",
+        200: "toonie",
+        100: "loonie",
+        25: "quarter",
+        10: "dime",
+        5: "nickel",
+        1: "penny",
+    }
+
+#create a empty dictionary to store the amounts of change
+
+
